50d5097e79773355d2ee52dfd3b096d7e5df7281
Gustavo Delgado <gustavodelgado@pm.me>
2019-08-03 11:13:03 -0300

    exercicio 2.6

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,19 +2,32 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
-public class Sessao {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+@Entity
+public class Sessao {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private LocalTime horario;
+	@ManyToOne
 	private Sala sala;
+	@ManyToOne
 	private Filme filme;
 	
-	public Sessao(Integer id, LocalTime horario, Sala sala, Filme filme) {
-		super();
-		this.id = id;
+	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
 		this.sala = sala;
 		this.filme = filme;
+	}
+	
+	public Sessao () {
+		
 	}
 
 	public Integer getId() {
