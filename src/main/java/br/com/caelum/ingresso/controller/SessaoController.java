f5eb8c3b97cc2b0932d91eb9166d3ff83c5eda8c
Gustavo Delgado <gustavodelgado@pm.me>
2019-08-03 16:47:11 -0300

    ex 2.12

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -45,16 +45,20 @@ public class SessaoController {
 	@PostMapping("/admin/sessao")
 	@Transactional
 	public ModelAndView salva(@Valid SessaoForm form, BindingResult result) {
+		
 		if (result.hasErrors())
 			return form(form.getSalaId(), form);
+		
 		Sessao sessao = form.toSessao(salaDao, filmeDao);
+		
 		List<Sessao> sessoesDaSala = sessaoDao.buscaSessoesDaSala(sessao.getSala());
 		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoesDaSala);
+		
 		if (gerenciador.cabe(sessao)) {
 			sessaoDao.save(sessao);
 			return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
 		}
-		
+
 		return form(form.getSalaId(), form);
 	}
 }
